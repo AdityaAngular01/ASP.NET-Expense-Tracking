025b5a808d16c37fc7f8ab6283238332d9786e28
AdityaAngular01 <aditya@angularminds.in>
2026-04-08 18:23:23 +0530

    feat: added user service and repository

---
+++ SmartExpense.API/Repositories/IUserRepository.cs
@@ -0,0 +1,23 @@
+
+
+using SmartExpense.API.DTOs;
+using SmartExpense.API.DTOs.Responses;
+using SmartExpense.API.Models;
+
+namespace SmartExpense.API.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        
+        Task AddAsync(User user);
+        
+        Task<User?> GetByIdAsync(int id);
+
+        Task<PaginationDataDTO<User>> GetAllAsync(QueryParams queryParams);
+        
+        Task<bool> DeleteByIdAsync(User user);
+        
+        Task<User?> UpdateAsync(User user);
+    }
+}
---
+++ SmartExpense.API/Services/UserService.cs
@@ -0,0 +1,4 @@
+namespace SmartExpense.API.Services
+{
+    
+}
