2655b1f3aa54e0d29fc0d5fd975908fb234215ba
AdityaAngular01 <aditya@angularminds.in>
2026-04-13 18:14:18 +0530

    Initial Commit

--- SmartExpense.Client/src/app/app.routes.ts
+++ SmartExpense.Client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+import { Features } from './pages/features/features';
+import { Pricing } from './pages/pricing/pricing';
+import { About } from './pages/about/about';
+import { Login } from './pages/Auth/login/login';
+import { Register } from './pages/Auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'features', component: Features },
+  { path: 'pricing', component: Pricing },
+  { path: 'about', component: About },
+
+  { path: 'login', component: Login },
+  { path: 'signup', component: Register },
+
+  { path: '**', redirectTo: '' },
+];
